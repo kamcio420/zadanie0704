5d2b5c6c86c11767e5d9ed712ebb9e1aa9977955
kamcio420 <kamcio420@gmail.com>
2022-04-07 13:09:31 +0200

    final

--- app/src/main/java/com/example/zadanie0704/MainActivity.kt
+++ app/src/main/java/com/example/zadanie0704/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.zadanie0704
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btn = findViewById<Button>(R.id.button)
+        val iv = findViewById<ImageView>(R.id.imageView)
+        var x = 1
+        btn.setOnClickListener {
+            if (x == 1) {
+                iv.setImageResource(R.drawable.img1)
+                x = 0
+            }
+            else if (x == 0) {
+                iv.setImageResource(R.drawable.img2)
+                x = 1
+            }
+        }
     }
 }
